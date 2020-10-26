d02b44c17b3dcd790270ec9fb108d9f2a243f63b
Aru123458 <73472383+Aru123458@users.noreply.github.com>
2020-10-26 16:47:06 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -72,7 +72,7 @@ function draw() {
   //trex.debug = true;
   background(255);
   text("Score: "+ score, 500,50);
-  text("High score"+localStorage["HighestScore"],410,50);
+  text("High score"+A["HighestScore"],410,50);
   if (gameState===PLAY){
     score = score + Math.round(getFrameRate()/60);
     ground.velocityX = -(6 + 3*score/100);
@@ -185,8 +185,8 @@ function reset(){
   
   trex.changeAnimation("running",trex_running);
   
-  if(localStorage["HighestScore"]<score){
-    localStorage["HighestScore"] = score;
+  if(a["HighestScore"]<score){
+    a["HighestScore"] = score;
   }
 
   
